911f2edef9d4f37a9f65f786d232a4174c3207a1
vincentlel <48972214+vincentlel@users.noreply.github.com>
2024-03-28 18:11:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,3 +19,28 @@ resource "aws_ecr_repository" "api" {
     scan_on_push = true
   }
 }
+# --- Build & push image ---
+
+locals {
+  repo_url = aws_ecr_repository.api.repository_url
+}
+
+resource "null_resource" "image" {
+  triggers = {
+    hash = md5(join("-", [for x in fileset("", "./code/{*.py,*.txt,Dockerfile}") : filemd5(x)]))
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      aws ecr get-login-password | docker login --username AWS --password-stdin ${local.repo_url}
+      docker build --platform linux/amd64 -t ${local.repo_url}:latest ./code
+      docker push ${local.repo_url}:latest
+    EOF
+  }
+}
+
+data "aws_ecr_image" "latest" {
+  repository_name = aws_ecr_repository.api.name
+  image_tag       = "latest"
+  depends_on      = [null_resource.image]
+}
