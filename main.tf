2aa4e9b8987a5205eda50bbfc54ea83ac076e7a3
vincentlel <48972214+vincentlel@users.noreply.github.com>
2024-03-28 18:17:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
+
 # --- ECR ---
 
 resource "aws_ecr_repository" "api" {
@@ -19,6 +20,7 @@ resource "aws_ecr_repository" "api" {
     scan_on_push = true
   }
 }
+
 # --- Build & push image ---
 
 locals {
@@ -44,3 +46,68 @@ data "aws_ecr_image" "latest" {
   image_tag       = "latest"
   depends_on      = [null_resource.image]
 }
+# --- IAM Role ---
+
+resource "aws_iam_role" "lambda" {
+  name = "lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Action    = "sts:AssumeRole"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+# --- Lambda ---
+
+resource "aws_cloudwatch_log_group" "api" {
+  name              = "/aws/lambda/api"
+  retention_in_days = 14
+}
+
+resource "aws_lambda_function" "api" {
+  function_name    = "api"
+  role             = aws_iam_role.lambda.arn
+  image_uri        = "${aws_ecr_repository.api.repository_url}:latest"
+  package_type     = "Image"
+  source_code_hash = trimprefix(data.aws_ecr_image.latest.id, "sha256:")
+  timeout          = 10
+
+  environment {
+    variables = {}
+  }
+
+  depends_on = [
+    null_resource.image,
+    aws_iam_role_policy_attachment.lambda_logs,
+    aws_cloudwatch_log_group.api,
+  ]
+}
+
+# --- Lambda Endpoint ---
+
+resource "aws_lambda_function_url" "api" {
+  function_name      = aws_lambda_function.api.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["*"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 86400
+  }
+}
+
+output "api_url" {
+  value = aws_lambda_function_url.api.function_url
+}
+
