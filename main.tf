7b14b359c507d6ba6e2214251bb40fa51ce45e69
vincentlel <48972214+vincentlel@users.noreply.github.com>
2024-03-28 18:08:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,3 +8,14 @@ provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
+# --- ECR ---
+
+resource "aws_ecr_repository" "api" {
+  name                 = "lambda-api"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
